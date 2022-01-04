1d52ff95a579de13fb206a20630bd65b65c0b05c
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2022-01-04 22:19:08 +0530

    Update register.blade.php

--- resources/views/student/register.blade.php
+++ resources/views/student/register.blade.php
@@ -50,6 +50,11 @@
             </div>
       </div>
     </nav>
+
+    <br>
+    <br>
+    <br>
+    <br>
     <div class="container">
       <div class="row">
         <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
